56a7649168815a52697be319c6ebe7da6e3d886a
stevan <stevangrubac123@gmail.com>
2026-03-05 15:29:11 -0500

    try fix siging redirect issue

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,9 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@/lib/supabase/server";
 
 export async function GET(request: NextRequest) {
-  const { searchParams, origin } = request.nextUrl;
+  const { searchParams } = request.nextUrl;
   const code = searchParams.get("code");
   const redirectTo = searchParams.get("redirectTo") || "/dashboard";
+
+  const origin = process.env.NEXT_PUBLIC_BASE_URL || request.nextUrl.origin;
 
   if (code) {
     const supabase = createClient();
