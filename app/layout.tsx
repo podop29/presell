a35f0f36f4208ed542a85d89192e6a6e4e700c90
stevan <stevangrubac123@gmail.com>
2026-03-05 14:56:15 -0500

    get up google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata, Viewport } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
+
+const GA_ID = "G-ZJT1GK8M4J";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,6 +37,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased bg-[#0a0a0a] text-neutral-200 min-h-screen`}
       >
